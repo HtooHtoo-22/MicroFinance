4a4d3bba45c9eee360a70a5e5abc95da2be1c065
JsixW <winhtataung11@gmail.com>
2025-02-11 15:37:11 +0630

    commit

--- backend/src/main/java/com/microfinance/code/repository/CurrentAccountRepository.java
+++ backend/src/main/java/com/microfinance/code/repository/CurrentAccountRepository.java
@@ -1,16 +1,12 @@
 package com.microfinance.code.repository;
 
-import com.microfinance.code.model.CIF;
 import com.microfinance.code.model.CurrentAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface CurrentAccountRepository extends JpaRepository<CurrentAccount, Integer> {
-
     Optional<CurrentAccount> findByAccountId(String accountId);
 }
