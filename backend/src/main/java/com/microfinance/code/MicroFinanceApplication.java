28552c2943a57354d158a47eeaa66f4e80f84cb4
JsixW <winhtataung11@gmail.com>
2025-02-13 10:04:01 +0630

    Your commit message

--- backend/src/main/java/com/microfinance/code/MicroFinanceApplication.java
+++ backend/src/main/java/com/microfinance/code/MicroFinanceApplication.java
@@ -1,10 +1,8 @@
 package com.microfinance.code;
 
-import com.microfinance.code.model.Branch;
-import com.microfinance.code.model.CIF;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class MicroFinanceApplication {
